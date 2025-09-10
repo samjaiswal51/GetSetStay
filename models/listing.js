e2bce51400680ef807f2f03459e37101ae99c08c
Samarth Jaiswal <samjaiswal51@gmail.com>
2025-09-11 02:55:13 +0530

    solved basic problem

--- models/listing.js
+++ models/listing.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
+  title: { type: String },
+  description: { type: String},
   image: {
     url: {
       type: String,
@@ -14,9 +14,9 @@ const listingSchema = new Schema({
       type: String,
     },
   },
-  price: { type: Number, required: true },
-  location: { type: String, required: true },
-  country: { type: String, required: true },
+  price: { type: Number, min:0},
+  location: { type: String},
+  country: { type: String},
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
