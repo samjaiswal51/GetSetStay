da5eafc5c01f50969d66596ed11e26fcd8fb7738
Samarth Jaiswal <samjaiswal51@gmail.com>
2025-09-12 00:50:42 +0530

    added joi

---
+++ schema.js
@@ -0,0 +1,12 @@
+const Joi = require('joi');
+
+module.exports.listingSchema = Joi.object({
+    lsiting : Joi.object({
+        title : Joi.string().required(),
+        description : Joi.string().required(),
+        country : Joi.string().required(),
+        location : Joi.string().required(),
+        price : Joi.number().required().min(0),
+        image: Joi.string().allow("", null),
+    }).required()
+})
